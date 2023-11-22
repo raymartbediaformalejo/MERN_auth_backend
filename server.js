ffc680d28e87ab18b7c6671f84f92a15983b99b3
Raymart <raymartformalejo00@gmail.com>
2023-11-22 17:29:23 +0800

    Update server

--- server.js
+++ server.js
@@ -6,21 +6,12 @@ import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
-import cors from "cors";
 
 const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
-
-app.use(
-  cors({
-    origin: ["https://mern-auth-ashy.vercel.app", "http://localhost:2000"],
-    credentials: true,
-    optionsSuccessStatus: 200,
-  })
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -46,3 +37,11 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// app.use(
+//   cors({
+//     origin: ["https://mern-auth-ashy.vercel.app", "http://localhost:2000"],
+//     credentials: true,
+//     optionsSuccessStatus: 200,
+//   })
+// );
